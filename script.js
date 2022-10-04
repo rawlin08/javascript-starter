abfa5bea024824934ab7a0ec35b56bd9bd7a1409
Alec Rawlins <alec12999@yahoo.com>
2022-10-04 14:09:21 -0400

    completed part 1

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+
+// Numbers
+
+console.log(23 + 97);
+console.log(2 + 8);
+console.log(3 + 10);
+console.log(20 + 35);
+console.log(10 + 37);
+console.log(45 + 27);
+console.log(10 + 77);
+console.log((4 + 6 + 9) / 77);
+
+// Variables
+
+let a = 10;
+console.log(a);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
