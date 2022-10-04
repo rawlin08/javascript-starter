182a34f3c4cfb17277f94c5ff248cc6f603a8fe9
Alec Rawlins <alec12999@yahoo.com>
2022-10-04 16:43:08 -0400

    cleaned up part 2 js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Part 1
+
 console.log("Hello, World!");
 
 // Numbers
@@ -19,3 +21,71 @@ console.log(a);
 const max = 57;
 const actual = max - 13;
 const percentage = actual / max;
+
+// Part 2
+
+// Troubleshooting
+
+function troubleshooting() {
+    const a = 1;
+	const b = 1;
+
+    console.log(a);
+    console.log(b);
+
+	let result;
+
+	result = a + b;
+
+    console.log(result);
+
+	return result;
+}
+
+// Number Checker
+
+function numberChecker() {
+
+    number = Number(prompt("enter a number"));
+
+    if(number >= 10) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+// Math
+
+const c = 1 + 8; // 9
+const d = 22 * 3; // 66
+const e = 5 % 4; // 1
+const f = c - 17; // -8
+const g = c + d + e + f; // 68
+
+function math() {
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
+    console.log(g);
+}
+
+// Follow
+
+const birthYear = 1948;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+function follow() {
+	const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+
+	console.log(greeting);
+
+	const improvedgreeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
+	console.log(improvedgreeting);
+}
