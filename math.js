15b2b014c3ec7c269f2c69d2e3db7f4a397f776b
Alec Rawlins <alec12999@yahoo.com>
2022-10-04 15:10:56 -0400

    completed part 2

---
+++ math.js
@@ -0,0 +1,25 @@
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   Enter the operations, replacing the `"?"`, make the computer do the work for you. 
+ *   Do not manually enter the answer, for example: "one plus ten" would look like 1+10
+ * 
+ * a = one plus eight
+ * b = 22 times three
+ * c = the *remainder* of 5/4
+ * d = the variable 'a' minus 17
+ * e = the sum of the previous four variables
+ */
+
+ const a = 1 + 8; // 9
+ const b = 22 * 3; // 66
+ const c = 5 % 4; // 1
+ const d = a - 17; // -8
+ const e = a + b + c + d; // 68
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+ 
